cd635b24afa9b949e23e2e4364b014e13fcc775e
Yeremy Turcios <yturcios@ancestry.com>
2016-02-22 09:32:15 -0700

    Modified SlackNotificationService so it posts to an incoming-webhook

--- seyren-core/src/main/java/com/seyren/core/service/notification/SlackNotificationService.java
+++ seyren-core/src/main/java/com/seyren/core/service/notification/SlackNotificationService.java
@@ -15,36 +15,30 @@ package com.seyren.core.service.notification;
 
 import static com.google.common.collect.Iterables.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import net.gpedro.integrations.slack.SlackApi;
+import net.gpedro.integrations.slack.SlackMessage;
 import org.apache.commons.lang.StringUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.HttpClientUtils;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 import com.seyren.core.domain.Alert;
 import com.seyren.core.domain.Check;
 import com.seyren.core.domain.Subscription;
 import com.seyren.core.domain.SubscriptionType;
 import com.seyren.core.exception.NotificationFailedException;
 import com.seyren.core.util.config.SeyrenConfig;
+
 
 @Named
 public class SlackNotificationService implements NotificationService {
@@ -66,31 +60,12 @@ public class SlackNotificationService implements NotificationService {
 
     @Override
     public void sendNotification(Check check, Subscription subscription, List<Alert> alerts) throws NotificationFailedException {
-        String url = String.format("%s/services/T02563LQP/B0MLAS09K/35YbNye0eFR7Dbp5ESc44SXe", baseUrl);
-        HttpClient client = HttpClientBuilder.create().useSystemProperties().build();
-        HttpPost post = new HttpPost(url);
-        post.addHeader("accept", "application/json");
+        List<String> emojis = Lists.newArrayList(
+                Splitter.on(',').omitEmptyStrings().trimResults().split(seyrenConfig.getSlackEmojis())
+        );
 
-        List<BasicNameValuePair> parameters = new ArrayList<BasicNameValuePair>();
-        parameters.add(new BasicNameValuePair("payload", formatContent(check, subscription, alerts)));
-
-        try {
-            post.setEntity(new UrlEncodedFormEntity(parameters));
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.info("> parameters: {}", parameters);
-            }
-            HttpResponse response = client.execute(post);
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.info("> parameters: {}", parameters);
-                LOGGER.debug("Status: {}, Body: {}", response.getStatusLine(), new BasicResponseHandler().handleResponse(response));
-            }
-        } catch (Exception e) {
-            LOGGER.warn("Error posting to Slack", e);
-        } finally {
-            post.releaseConnection();
-            HttpClientUtils.closeQuietly(client);
-        }
-
+        SlackApi api = new SlackApi(subscription.getTarget());
+        api.call(new SlackMessage(formatContent(emojis, check, subscription, alerts)));
     }
 
     @Override
@@ -98,7 +73,8 @@ public class SlackNotificationService implements NotificationService {
         return subscriptionType == SubscriptionType.SLACK;
     }
 
-    private String formatContent(Check check, Subscription subscription, List<Alert> alerts) {
+    private String formatContent(List<String> emojis, Check check, Subscription subscription, List<Alert> alerts) {
+        String url = String.format("%s/#/checks/%s", seyrenConfig.getBaseUrl(), check.getId());
         String alertsString = Joiner.on("\n").join(transform(alerts, new Function<Alert, String>() {
             @Override
             public String apply(Alert input) {
@@ -117,9 +93,11 @@ public class SlackNotificationService implements NotificationService {
 
         final String state = check.getState().toString();
 
-        return String.format("%s* %s %s\n```\n%s\n```\n#%s %s",
+        return String.format("%s*%s* %s [%s]%s\n```\n%s\n```\n#%s %s",
+                Iterables.get(emojis, check.getState().ordinal(), ""),
                 state,
                 check.getName(),
+                url,
                 description,
                 alertsString,
                 state.toLowerCase(Locale.getDefault()),
